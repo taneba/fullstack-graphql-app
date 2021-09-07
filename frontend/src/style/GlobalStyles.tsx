e202733248dbf0261f288dd67453f2004c8ba0b4
taneba <taneba.vla@gmail.com>
2021-09-07 10:47:30 +0900

    Update GlobalStyles.tsx

--- frontend/src/style/GlobalStyles.tsx
+++ frontend/src/style/GlobalStyles.tsx
@@ -1,9 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
-import tw, { GlobalStyles as BaseStyles, theme } from 'twin.macro'
+import tw, { css, GlobalStyles as BaseStyles } from 'twin.macro'
 
 const CustomStyles = createGlobalStyle`
   body {
-    -webkit-tap-highlight-color: ${theme`colors.purple.500`};
     ${tw`antialiased`}
   }
 `
