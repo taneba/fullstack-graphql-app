1a8383208347db7188d3ad3072610c4e85ae5d35
taneba <taneba.vla@gmail.com>
2021-09-16 14:15:35 +0900

    fixed import path

--- frontend/src/pages/signout/index.page.tsx
+++ frontend/src/pages/signout/index.page.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react'
 
-import { Button } from '~/components/Button'
+import { Button } from '~/components'
 
 function SignOut() {
   const { logout } = useAuth0()
