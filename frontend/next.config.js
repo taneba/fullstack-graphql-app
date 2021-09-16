e98783e90e023ca3bf0141f97d34298fb7bebe71
taneba <taneba.vla@gmail.com>
2021-09-16 23:28:07 +0900

    add bundle analyzer

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const config = {
   pageExtensions: ['page.tsx', 'api.ts'],
 }
+
+module.exports = withBundleAnalyzer(config)
