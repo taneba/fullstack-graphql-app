eb73201b961cc42d93c206e2959c14016b355815
taneba <taneba.vla@gmail.com>
2022-04-06 07:50:29 +0900

    Suspense

--- apps/frontend/src/pages/users/index.page.tsx
+++ apps/frontend/src/pages/users/index.page.tsx
@@ -1,20 +1,12 @@
-import { Suspense, useState } from 'react'
+import { Suspense } from 'react'
 
-import { Spinner, TextField } from '~/components'
+import { Spinner } from '~/components'
 
 import { UserList } from './UserList'
 
 function Users() {
-  const [value, setValue] = useState('')
-
   return (
     <div tw="mt-4">
-      <TextField
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        name={'foo'}
-      />
-      <div>{value}</div>
       <h1 tw="text-black font-bold text-3xl">All Users</h1>
       <Suspense fallback={<Spinner />}>
         <UserList />
