31397db77d27c88ca33096d61ff0eecfa9714469
taneba <taneba.vla@gmail.com>
2023-01-28 10:10:53 +0900

    fixed no unknown property error

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const base = require('config/exlint-next')
+
 module.exports = {
-  ...require('config/exlint-next'),
+  ...base,
+  rules: {
+    ...base.rules,
+    'react/no-unknown-property': ['error', { ignore: ['css', 'tw'] }],
+  },
 }
