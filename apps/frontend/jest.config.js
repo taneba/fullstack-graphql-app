7e034a00a13f4c7e8eb90e6b1969b929b41f6d32
taneba <taneba.vla@gmail.com>
2023-02-06 11:10:41 +0900

    misc

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -6,10 +6,21 @@ module.exports = {
   ],
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  preset: 'ts-jest',
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
+    '^ui$': '<rootDir>/node_modules/ui/src/index.tsx',
   },
   moduleDirectories: ['node_modules', '<rootDir>'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/test/__fixtures__',
+    '<rootDir>/node_modules',
+    '<rootDir>/dist',
+  ],
   globals: {
     'ts-jest': {
       tsconfig: {
